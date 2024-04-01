d5b6120a00ea0c194e6703d529b9a4e151fec70c
Ibwedagama <wardana.wedagama@gmail.com>
2024-04-02 01:05:05 +0800

    feat: add type definition

---
+++ lib/colors.types.ts
@@ -0,0 +1,34 @@
+export type ColorShades = {
+  50: string
+  100: string
+  200: string
+  300: string
+  400: string
+  500: string
+  600: string
+  700: string
+  800: string
+  900: string
+}
+
+export type Colors = {
+  yellow: ColorShades
+  green: ColorShades
+  blue: ColorShades
+  pink: ColorShades
+  red: ColorShades
+  purple: ColorShades
+  gray: ColorShades
+  bluegray: ColorShades
+}
+
+export type ColorsWithOverride = {
+  'jds-yellow': ColorShades
+  'jds-green': ColorShades
+  'jds-blue': ColorShades
+  'jds-pink': ColorShades
+  'jds-red': ColorShades
+  'jds-purple': ColorShades
+  'jds-gray': ColorShades
+  'jds-bluegray': ColorShades
+}
