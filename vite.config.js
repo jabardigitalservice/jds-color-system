04c065c436971f39c945ad89fcb6e14956a349c9
Ibwedagama <wardana.wedagama@gmail.com>
2024-04-02 15:48:22 +0800

    update (vite.config): add minify option; adjust formats

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,10 @@ const { defineConfig } = require('vite')
 module.exports = defineConfig({
   build: {
     lib: {
+      minify: true,
       entry: path.resolve(__dirname, 'lib/main.ts'),
       name: 'jds-color-system',
+      formats: ['es', 'umd'],
       fileName: (format) => `jds-color-system.${format}.js`
     }
   }
