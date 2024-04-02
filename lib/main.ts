d42d7d4fd5835d0cfb12aae550a2ba3dc080720f
Ibwedagama <wardana.wedagama@gmail.com>
2024-04-02 10:13:39 +0800

    feat: add jsdoc to main.ts file

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,12 @@
 import type { Colors, ColorsWithPrefix } from './colors.types'
 import colors from './colors'
 
+/**
+ * Generate colors from JDS Color System.
+ *
+ * @param {Colors} colorsObj - color bank object
+ * @returns {ColorsWithPrefix}
+ */
 function generateColors(colorsObj: Colors) {
   const result: Partial<ColorsWithPrefix> = {}
 
@@ -15,6 +21,13 @@ type Config = {
   override?: boolean
 }
 
+/**
+ * Extend or Override Tailwind CSS Colors with JDS Color System.
+ * To override Tailwind CSS, pass param `override: true`.
+ *
+ * @param {Config} config
+ * @returns {Colors | ColorsWithPrefix}
+ */
 export default function (config: Config = { override: false }) {
   if (config.override) return colors
 
